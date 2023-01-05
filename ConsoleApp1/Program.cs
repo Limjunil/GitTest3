91f6170fa0fa86ce33e123071e466bb342a39a4f
DESKTOP-ILDHSHN\KGA_11 <im5820795@naver.com>
2023-01-05 09:45:42 +0900

    [Junil] Ruler project setup ok

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,11 +2,33 @@
 
 namespace ConsoleApp1
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Ruler ruler = new Ruler(10);
+            ruler.Run();
         }
     }
+
+    public class Ruler
+    {
+        private const float ONE_INCH = 2.54f;
+
+        public int Centimeter { get; set; } = 0;
+
+        public float Inch
+        {
+            get { return Centimeter * ONE_INCH; }
+            private set { Centimeter = (int)(value / ONE_INCH); }
+        }
+
+        // 생성자
+        public Ruler(int cmValue) { Centimeter = cmValue; }
+
+        public void Run()
+        {
+            Console.WriteLine($"{Centimeter} cm는 {Inch} inch 입니다.");
+        }
+    } // class Ruler
 }
